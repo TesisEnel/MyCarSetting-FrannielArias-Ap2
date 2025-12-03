e8118136948e9e32388b3785fa0926e2b3cb920c
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-12-03 03:32:26 -0400

    refactor ♻️ (history): Change OnDeleteRecord id from String to Int
    This commit modifies the `MaintenanceHistoryEvent` sealed interface.
    
    The `id` property within the `OnDeleteRecord` data class has been changed from a `String` to an `Int` to align with the data type of the record's identifier.

--- app/src/main/java/edu/ucne/loginapi/presentation/maintenanceHistory/MaintenanceHistoryEvent.kt
+++ app/src/main/java/edu/ucne/loginapi/presentation/maintenanceHistory/MaintenanceHistoryEvent.kt
@@ -5,7 +5,10 @@ import edu.ucne.loginapi.domain.model.MaintenanceType
 sealed interface MaintenanceHistoryEvent {
     object LoadInitialData : MaintenanceHistoryEvent
     object Refresh : MaintenanceHistoryEvent
-    data class OnDeleteRecord(val id: String) : MaintenanceHistoryEvent
+
+    data class OnDeleteRecord(val id: Int) : MaintenanceHistoryEvent
+
     data class OnTypeFilterSelected(val type: MaintenanceType?) : MaintenanceHistoryEvent
+
     object OnUserMessageShown : MaintenanceHistoryEvent
 }
